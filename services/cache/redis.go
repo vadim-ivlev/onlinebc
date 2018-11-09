bda8ee763236fcb1fb4a57b36b19176557970d37
Ивлев Вадим <ivlev@rg.ru>
2018-11-09 16:58:29 +0300

    add: adding Redis

--- services/cache/redis.go
+++ services/cache/redis.go
@@ -1,13 +1,25 @@
 package cache
 
 import (
+	"log"
 	"errors"
+	"github.com/mediocregopher/radix.v2/redis"
 )
 
 // Get value by key.
 // If key is absent returns an empty string and an error.
 func Get(key string) (string, error) {
-	return "", errors.New("Not implemented")
+	conn, err := redis.Dial("tcp", "localhost:6379")
+    if err != nil {
+        return "", err
+	}
+	defer conn.Close()
+
+	str, err := conn.Cmd("HGET", "album:1", "title").Str()
+    if err != nil {
+        return "", err
+    }
+	return str, nil
 }
 
 // Sets value by key.
