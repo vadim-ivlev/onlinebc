bcf59b87be5b87be0551049f0670ce84a9a7c320
Ивлев Вадим <ivlev@rg.ru>
2018-11-30 18:25:17 +0300

    fix: create-functions

--- models/db-scripts/create-functions.sql
+++ models/db-scripts/create-functions.sql
@@ -1,149 +1,47 @@
 /*
 
-## Functions and views to represent tables as JSONs
-
+## Functions to represent tables as JSONs
 
 */
 
-DROP VIEW IF EXISTS broadcast_view;
-
-CREATE VIEW broadcast_view AS
-    SELECT
-        id,
-        title name,
-        is_ended,
-
-        to_char(to_timestamp(time_begin::double precision), 'DD.MM.YYYY'::text)    AS date,
-        to_char(to_timestamp(time_begin::double precision), 'HH24:MI'::text)       AS time,
-
-
-         "info": "Трансляция завершена",
-
-         link_article uri,
-         "media": { "uri": "" },
-         show_date,
-         show_time,
-         is_diary diary,
-         diary_author,
-
-         "posts": [],
-         "posts__length": 85
-
-
-
-
-
-
-
-
-
-
-
-        id,
-        id_parent,
-        id_broadcast,
-        has_big_img,
-        author          posts__author,
-        "text"          posts__text,
-        post_type       posts__type,
-        link            posts__uri,
-        to_char(to_timestamp(post_time::double precision), 'DD.MM.YYYY'::text)    AS posts__date,
-        to_char(to_timestamp(post_time::double precision), 'HH24:MI'::text)       AS posts__time,
-        get_media(id)   posts__media,
-        ('[]')::json    posts__answers
-    FROM
-        broadcast;
-
-
-
-
--- post_view: has the same fields as rg.ru Json API
-
-DROP VIEW IF EXISTS post_view;
-
-CREATE VIEW post_view AS
-    SELECT
-        id,
-        id_parent,
-        id_broadcast,
-        has_big_img,
-        author          posts__author,
-        "text"          posts__text,
-        post_type       posts__type,
-        link            posts__uri,
-        to_char(to_timestamp(post_time::double precision), 'DD.MM.YYYY'::text)    AS posts__date,
-        to_char(to_timestamp(post_time::double precision), 'HH24:MI'::text)       AS posts__time,
-        get_media(id)   posts__media,
-        ('[]')::json    posts__answers
-    FROM
-        post;
-
-
--- answer_view: has the same fields as rg.ru Json API
-
-DROP VIEW IF EXISTS answer_view;
-
-CREATE VIEW answer_view AS
-    SELECT
-        id,
-        id_parent,
-        id_broadcast,
-        has_big_img,
-        author          posts__answer__author,
-        "text"          posts__answer__text,
-        post_type       posts__answer__type,
-        link            posts__answer__uri,
-        to_char(to_timestamp(post_time::double precision), 'DD.MM.YYYY'::text)    AS posts__answer__date,
-        to_char(to_timestamp(post_time::double precision), 'HH24:MI'::text)       AS posts__answer__time,
-        get_media(id)   posts__answer__media,
-        ('[]')::json    posts__answer__answers
-    FROM
-        post;
-
-
-
-
-
-
-
--- get_media:  returns media json by post id --------------------------------------
+-- get_media:  returns media json by post id ------------------------------------------
 
 DROP FUNCTION IF EXISTS get_media(integer);
 
 CREATE OR REPLACE FUNCTION get_media(idd integer) RETURNS json AS $$
-BEGIN
-    RETURN
+BEGIN   
+    RETURN  
     (
-        select array_to_json(array_agg(row_to_json( t, false )),true) from
-        (
-            SELECT * FROM media
-            WHERE post_id = idd
-        ) t
+        select array_to_json(array_agg(row_to_json( t, false )),true) from 
+        ( 
+            SELECT 
+                id,
+                id_parent,
+                id_broadcast,
+                author          posts__author,
+                "text"          posts__text,
+                post_type       posts__type,
+                link            posts__uri,
+                to_char(to_timestamp(post_time::double precision), 'DD.MM.YYYY'::text)    AS posts__date,
+                to_char(to_timestamp(post_time::double precision), 'HH24:MI'::text)       AS posts__time,
+                ('[]')::json    posts__media,
+                ('[]')::json    posts__answers
+            FROM 
+                post 
+            WHERE 
+                    id_broadcast = idd 
+                AND id_parent IS NULL 
+        ) t 
     );
-
+    
 END;
 $$ LANGUAGE plpgsql;
 
+-- test
+--SELECT get_posts(354);
+SELECT jsonb_pretty( get_posts(354)::jsonb);
 
 
-
--- get_answers:  returns json of posts(answers) by post(questions) id ------------------------------------------
-
-DROP FUNCTION IF EXISTS get_answers(integer);
-
-CREATE OR REPLACE FUNCTION get_answers(idd integer) RETURNS json AS $$
-BEGIN
-    RETURN
-    (
-        select array_to_json(array_agg(row_to_json( t, false )),true) from
-        (
-            SELECT * FROM answer_view
-            WHERE id_parent = idd
-        ) t
-    );
-
-END;
-$$ LANGUAGE plpgsql;
 
 
 
@@ -157,18 +55,36 @@ CREATE OR REPLACE FUNCTION get_posts(idd integer) RETURNS json AS $$
 BEGIN   
     RETURN  
     (
-        select array_to_json(array_agg(row_to_json( t, false )),true) from
+        select array_to_json(array_agg(row_to_json( t, false )),true) from 
         ( 
-            SELECT * FROM post_view
+            SELECT 
+                id,
+                id_parent,
+                id_broadcast,
+                author          posts__author,
+                "text"          posts__text,
+                post_type       posts__type,
+                link            posts__uri,
+                to_char(to_timestamp(post_time::double precision), 'DD.MM.YYYY'::text)    AS posts__date,
+                to_char(to_timestamp(post_time::double precision), 'HH24:MI'::text)       AS posts__time,
+                ('[]')::json    posts__media,
+                ('[]')::json    posts__answers
+            FROM 
+                post 
             WHERE 
                     id_broadcast = idd 
-                AND id_parent IS NULL
-            ORDER BY id DESC
+                AND id_parent IS NULL 
         ) t 
     );
     
 END;
 $$ LANGUAGE plpgsql;
+
+-- test
+--SELECT get_posts(354);
+SELECT jsonb_pretty( get_posts(354)::jsonb);
+
+
 
 
 
@@ -180,26 +96,22 @@ DROP FUNCTION IF EXISTS get_broadcast(integer);
 
 CREATE OR REPLACE FUNCTION get_broadcast(idd integer) RETURNS json AS $$
 BEGIN   
-    RETURN
+    RETURN  
     (
-        select row_to_json(t) from
-        ( select *, get_posts(id) as posts  from broadcast where id = idd ) t
+        select row_to_json(t, true)
+        from 
+        ( select *, get_posts(id) as posts  from broadcast where id = idd ) t 
     );
+    
 END;
 $$ LANGUAGE plpgsql;
 
 
--- tests
-
-SELECT jsonb_pretty( get_media(5329)::jsonb);
-SELECT jsonb_pretty( get_answers(23929)::jsonb);
--- SELECT jsonb_pretty( get_post(354)::jsonb);
-SELECT jsonb_pretty( get_posts(354)::jsonb);
+-- test 
+SELECT get_broadcast(354);
 SELECT jsonb_pretty( get_broadcast(354)::jsonb);
 
 
 
-
--- the following expressions are equal ???
--- (select row_to_json(t)                                          from (...) t )
--- (select array_to_json(array_agg(row_to_json( t, false )),false) from (...) t )
+SELECT jsonb_pretty ( (broadcast->'online'->'broadcast'->0->'posts') ::jsonb ) "json" FROM broadcast_json WHERE id=247;
+SELECT jsonb_pretty ( get_posts(247)::jsonb);
